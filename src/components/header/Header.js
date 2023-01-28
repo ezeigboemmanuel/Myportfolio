f8fde360cb5a32776708d66a0f2a6dc97b882871
ezeigboemmanuel <ezeigboemmanuel2@gmail.com>
2023-01-28 08:58:21 -0600

    worked on the header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./header.css"
+import CTA from "./CTA"
+import myimg from "../../assets/senddp.png"
 
 function Header(){
     return(
         <div>
-            <h1>Header</h1>
+            <header>
+                <div className = "container header_container">
+                    <h5>Hello I'm</h5>
+                    <h1>Ezeigbo Emmanuel</h1>
+                    <h5 className = "text-light">Frontend Developer</h5>
+                    <CTA />
+
+                    
+                    <div className = "me">
+                        <img src = {myimg} alt = "" />
+                    </div>
+
+                    <a href = "#contact" className = "scroll_down">Scroll Down</a>
+                </div>
+            </header>
         </div>
     )
 }
